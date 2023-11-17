a52d9cd92e12c8aa27e13ca09ad69319d7ece8d8
bence0601 <nagybence0601@gmail.com>
2023-11-17 12:18:40 +0100

    Commit : Added A Logger inside the Get request

--- WeatherForeCast/WeatherForeCast/Controllers/WeatherForecastController.cs
+++ WeatherForeCast/WeatherForeCast/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace WeatherForeCast.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Received a request for weather forecast");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
