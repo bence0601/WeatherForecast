249a983b177e0be18f3614c7d6c7c5d2e5732a9b
bence0601 <nagybence0601@gmail.com>
2023-11-17 15:00:33 +0100

    Commit : GetCurrent method implemented

--- WeatherForeCast/WeatherForeCast/Controllers/WeatherForecastController.cs
+++ WeatherForeCast/WeatherForeCast/Controllers/WeatherForecastController.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
 
 namespace WeatherForeCast.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -38,6 +41,46 @@ namespace WeatherForeCast.Controllers
                 .ToArray();
 
             return Ok();
+        }
+
+
+        [HttpGet("GetCurrent")]
+        public WeatherForecast Getcurrent()
+        {
+            var apiKey = "410b77bb7ea5e4483af51a593d71c09d";
+
+            var lat = 47.497913;
+            var lon = 19.040236;
+            var url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+
+            var client = new WebClient();
+
+            _logger.LogInformation("Calling OpenWeather API with url : {url}", url);
+            var weatherData = client.DownloadString(url);
+
+            return ProcessJsonResponse(weatherData);
+        }
+        private static WeatherForecast ProcessJsonResponse(string weatherData)
+        {
+            JsonDocument json = JsonDocument.Parse(weatherData);
+            JsonElement main = json.RootElement.GetProperty("main");
+            JsonElement weather = json.RootElement.GetProperty("weather")[0];
+
+            WeatherForecast forecast = new WeatherForecast
+            {
+                Date = GetDateTimeFromUnixTimeStamp(json.RootElement.GetProperty("dt").GetInt64()),
+                TemperatureC = (int)main.GetProperty("temp").GetDouble(),
+                Summary = weather.GetProperty("description").GetString()
+            };
+
+            return forecast;
+        }
+        private static DateTime GetDateTimeFromUnixTimeStamp(long timeStamp)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(timeStamp);
+            DateTime dateTime = dateTimeOffset.UtcDateTime;
+
+            return dateTime;
         }
     }
 }
